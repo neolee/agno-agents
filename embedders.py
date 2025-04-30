e6d7c2c54348a087b81ac61d457010dbd8c088a7
Neo Lee <neo.lee@gmail.com>
2025-04-30 22:31:01 +0800

    refactor: update and use new mal

--- embedders.py
+++ embedders.py
@@ -3,9 +3,6 @@ from agno.embedder.openai import OpenAIEmbedder
 from mal.providers import Provider, provider_by_alias
 
 
-# s = "The quick brown fox jumps over the lazy dog."
-s = "路漫漫其修远兮，吾将上下而求索"
-
 def test_embedder(s, embedder):
     embeddings = embedder.get_embedding(s)
     print(f"Embeddings: {embeddings[:5]}")
@@ -25,8 +22,11 @@ def test_openai_compatible_embedder(s: str, provider: Provider, model_id: str, d
     test_embedder(s, embedder)
 
 
-test_ollama_embedder(s, model_id="nomic-embed-text", dimensions=768)
-test_ollama_embedder(s, model_id="snowflake-arctic-embed2", dimensions=1024)
+# s = "The quick brown fox jumps over the lazy dog."
+s = "路漫漫其修远兮，吾将上下而求索"
 
+test_ollama_embedder(s, model_id="nomic-embed-text", dimensions=768)
+local = provider_by_alias("local")
+test_openai_compatible_embedder(s, local, model_id="snowflake", dimensions=1024)
 qwen = provider_by_alias("qwen")
 test_openai_compatible_embedder(s, qwen, model_id="text-embedding-v3", dimensions=1024)
