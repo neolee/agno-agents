5a3f5548008a44d0e1c6f1d723e70858fe60894b
Neo Lee <neo.lee@gmail.com>
2026-01-20 16:03:37 +0800

    env: update mal provider config

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
-from agno.models.ollama.chat import Ollama
 from agno.models.deepseek.deepseek import DeepSeek
 
-from mal.providers import deepseek_provider, ollama_provider
+from mal.providers import deepseek_provider
 from mal.adapter.agno import model
 
 
@@ -11,14 +10,10 @@ deepseek_reasoner = DeepSeek(id=deepseek_provider.reasoner_model_id)
 qwen = model("qwen/qwen-plus-latest")
 qwen_coder = model("qwen/qwen3-coder-plus")
 
-openrouter_gemini_flash = model("openrouter/google/gemini-2.5-flash")
+openrouter_gemini_flash = model("openrouter/google/gemini-3-flash-preview")
 openrouter_gemini_pro = model("openrouter/google/gemini-3-pro-preview")
 
 local = model("local")
-
-lmstudio = model("lmstudio")
-
-ollama = Ollama(ollama_provider.model_id)
 
 default = qwen
 default_reasoner = deepseek_reasoner
