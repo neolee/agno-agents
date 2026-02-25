a75ad56be24e0d3405d785fc1fca8e6d549c3f7a
Neo Lee <neo.lee@gmail.com>
2026-02-25 18:17:43 +0800

    env: update mal and all packages

--- models.py
+++ models.py
@@ -7,13 +7,9 @@ from mal.adapter.agno import model
 deepseek = DeepSeek(id=deepseek_provider.model_id)
 deepseek_reasoner = DeepSeek(id=deepseek_provider.reasoner_model_id)
 
-qwen = model("qwen/qwen-plus-latest")
-qwen_coder = model("qwen/qwen3-coder-plus")
+qwen = model("qwen/qwen3.5-plus")
 
 gpt = model("openai")
-
-openrouter_gemini_flash = model("openrouter/google/gemini-3-flash-preview")
-openrouter_gemini_pro = model("openrouter/google/gemini-3-pro-preview")
 
 local = model("local")
 
